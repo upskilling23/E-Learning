513111082276464c1d63813062322162274b7b1c
rithick <rithick03krishna@gmail.com>
2024-12-23 17:31:02 +0530

    feat(e-portal): formatted backend components

--- BE-Node/App.js
+++ BE-Node/App.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use("/user", userRoutes);
 
 app.listen(process.env.PORT, () =>
-  console.log(`server running on port ${process.env.PORT}`)
+  console.log(`server running on port ${process.env.PORT}`),
 );
